6bb9ac362240a3b4d41fbb1c22c71b91ccfad2f9
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2022-04-18 01:23:59 -0300

    Improve command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,72 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
 use std::path::PathBuf;
-
-use clap::{Parser, Subcommand};
+use clap::{Parser, Args, Subcommand};
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
-#[clap(author, version, about)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
 struct Cli {
-    /// Turn debugging information on
+    /// turn debugging information on
     #[clap(short, long, parse(from_occurrences))]
     debug: usize,
 
     #[clap(subcommand)]
-    command: Option<Commands>,
+    subcommand: Option<SubCommands>,
+}
+
+#[derive(Args)]
+struct Add {
+    /// local yas repository to add files to 
+    repo: String,
+    /// The added files will be encrypted in the yas local and remote repositories
+    #[clap(short, long)]
+    recursive : bool,
+    /// The added files will be encrypted in the yas local and remote repositories
+    #[clap(short, long)]
+    encrypt : bool,
+    /// Files to add
+    paths: Vec<std::path::PathBuf>,
 }
 
 #[derive(Subcommand)]
-enum Commands {
-    /// does testing things
-    Add {
-        /// files
-        #[clap(required = true)]
-        files: Vec<String>,
-    },
+enum SubCommands {
+    /// add a file to a local yas repository 
+    Add(Add),
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    // if let Some(config_path) = cli.config.as_deref() {
-    //     println!("Value for config: {}", config_path.display());
+    // // You can see how many times a particular flag or argument occurred
+    // // Note, only flags can have multiple occurrences
+    // match cli.debug {
+    //     0 => println!("Debug mode is off"),
+    //     1 => println!("Debug mode is kind of on"),
+    //     2 => println!("Debug mode is on"),
+    //     _ => println!("Don't be crazy"),
     // }
 
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Add { files }) => {
-            println!("{:?}",files);
-        }
+    match &cli.subcommand {
+        Some(SubCommands::Add(args)) => add(args)?,
         None => {}
     }
+    Ok(())
+}
 
-    // Continued program logic goes here...
+fn add(args : &Add) -> Result<(), anyhow::Error> {
+    println!("{} {} {:?}", args.repo, args.encrypt, args.paths);
+
+    for path in &args.paths {
+        let f = File::open(path)
+              .with_context(|| format!("Could not open file `{}`", path.display()))?;
+        let f = BufReader::new(f);
+
+        // for line in f.lines() {
+        //   println!("{}", line.unwrap());
+        // }
+    }
+    Ok(())
 }
