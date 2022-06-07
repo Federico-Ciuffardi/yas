5227140562cf17aaf32a243b4e7006d9126df4ec
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2022-06-07 12:33:36 -0300

    Update GitRemote.h

--- src/git/GitRemote.h
+++ src/git/GitRemote.h
@@ -19,11 +19,11 @@ private:
 // functions
 public:
   GitRemote(url u, git_direction dir);
-  GitRemote(git_repository *repo, git_direction dir);
   vector<const git_remote_head*> ls();
   ~GitRemote();
 
 private:
+  GitRemote(git_repository *repo, git_direction dir);
   GitRemote();
 
 friend GitRepo;
